2d0f03276a84983772c71c319da28690c74347e5
ProtoSeo <proto_seo@naver.com>
2023-08-24 00:21:34 +0900

    init project

---
+++ src/main/java/com/example/hellostore/HelloStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellostore/HelloStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
